5d59e4343ab2a2df73d1dde38a0776bef4ebc4b5
Min Khant Thaw <mkt293822@gmail.com>
2026-03-30 19:28:49 +0630

    Update _layout.tsx
    Remove update loading.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -39,9 +39,9 @@ export default function RootLayout() {
 		// checkUpdate();
 	}, []);
 
-	if (isUpdating) {
-		return <Loading message="Checking for update" />;
-	}
+	//if (isUpdating) {
+		//return <Loading message="Checking for update" />;
+	//}
 
 	return (
 		<>
